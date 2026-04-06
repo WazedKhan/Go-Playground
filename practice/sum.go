723bec3ee83724fdc5f559a157c1db051134f4ca
wazedkhan <wazedkhan119399@gmail.com>
2026-04-06 16:11:39 +0600

    feat: sums tails of slice

--- practice/sum.go
+++ practice/sum.go
@@ -16,3 +16,12 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return res
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers[1:]))
+	}
+	return sums
+}
