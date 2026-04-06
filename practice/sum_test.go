f61180d11215a3a78808e9426adc0c1c83772e63
wazedkhan <wazedkhan119399@gmail.com>
2026-04-06 14:17:41 +0600

    feat: sum using slic

--- practice/sum_test.go
+++ practice/sum_test.go
@@ -4,11 +4,24 @@ import "testing"
 
 
 func TestSums(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, numbers)
-	}
+		want := 15
+		got := Sum(numbers)
+		if want != got {
+			t.Errorf("want %d got %d given %v", want , got, numbers)
+		}
+	})
+
+	t.Run("collection on any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		want := 6
+		got := Sum(numbers)
+
+		if want != got {
+			t.Errorf("want %d got %d given %v", want, got, numbers)
+		}
+	})
 }
