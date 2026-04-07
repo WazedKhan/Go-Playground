4c420cb450cbf594e3fb5d5917eb69642177f1f3
wazedkhan <wazedkhan119399@gmail.com>
2026-04-07 22:06:54 +0600

    With Test-02: handle runtime error with empty slice

--- practice/sum.go
+++ practice/sum.go
@@ -21,7 +21,11 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers[1:]))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
 	}
 	return sums
 }
--- practice/sum_test.go
+++ practice/sum_test.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"slices"
+	"reflect"
 	"testing"
 )
 
 func TestSums(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %d got %d", want, got)
+		}
+	}
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
@@ -31,17 +38,20 @@ func TestSums(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
 
-		if !slices.Equal(got, want) {
-			t.Errorf("got %d want %d", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("get sum of tails", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !slices.Equal(got, want) {
-			t.Errorf("got %d want %d", got, want)
-		}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
