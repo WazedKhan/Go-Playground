6d39dcf88737cda7a3f9246ae985a8b63ab52688
wazedkhan <wazedkhan119399@gmail.com>
2026-04-14 10:10:24 +0600

    feat: setting storage with todo

--- cli-tool/internal/todos.go
+++ cli-tool/internal/todos.go
@@ -1,8 +1,32 @@
 package internal
 
-import "cli-tool/models"
+import (
+	"bufio"
+	"os"
+	"time"
+
+	"cli-tool/models"
+	"cli-tool/repository"
+)
 
 // GetTodoList is a function that retrieves a list of TODO items. Currently, it returns an empty list and no error.
 func GetTodoList() ([]models.Todos, error) {
 	return []models.Todos{}, nil
+}
+
+func CreateTodos() error {
+	scanner := bufio.NewScanner(os.Stdin)
+	now := time.Now().Format("2006-01-02 15:04:05")
+	todo := models.Todos{
+		CreatedAt: now,
+		Status:    models.PENDING,
+	}
+	var title string
+	if scanner.Scan() {
+		title = scanner.Text()
+	}
+	todo.Title = title
+
+	repository.AddTODO(todo)
+	return nil
 }
---
+++ cli-tool/models/setting.go
@@ -0,0 +1,5 @@
+package models
+
+type Setting struct {
+	MaxTitleLength int `json:"maxTitleLength"`
+}
