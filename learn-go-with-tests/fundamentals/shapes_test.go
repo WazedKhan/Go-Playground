706a6b66c46af868e6d93c85e65191a5ec30e677
wazedkhan <wazedkhan119399@gmail.com>
2026-04-25 19:44:23 +0600

    learn(interface): introduction to table driven tests

--- learn-go-with-tests/fundamentals/shapes_test.go
+++ learn-go-with-tests/fundamentals/shapes_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -13,23 +15,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Helper()
-	checker := func(t testing.TB, shape Shape, want float64) {
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	// implementing table driven test
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		want := 72.0
-		checker(t, rectangle, want)
-	})
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checker(t, circle, want)
-	})
-
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want", got, tt.hasArea)
+			}
+		})
+	}
 }
