72a7f23f123ca48b6282ddcefa73535c75bbd6ec
wazedkhan <wazedkhan119399@gmail.com>
2026-05-06 17:33:36 +0600

    feat: delete from dictionary

--- learn-go-with-tests/fundamentals/dictionary_test.go
+++ learn-go-with-tests/fundamentals/dictionary_test.go
@@ -87,3 +87,29 @@ func TestUpdate(t *testing.T) {
 		assertError(t, err, ErrWordDoesNotExist)
 	})
 }
+
+func TestDelete(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "test definition"}
+
+		err := dictionary.Delete(word)
+
+		if err != nil {
+			t.Fatal("got an error but didn't wanted it")
+		}
+
+		_, err = dictionary.Search(word)
+
+		assertError(t, err, ErrNotFound)
+	})
+
+	t.Run("non-existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Delete(word)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
