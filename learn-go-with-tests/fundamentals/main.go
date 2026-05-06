907870de52ae8c52e71bab9061bfcc5486344c37
wazedkhan <wazedkhan119399@gmail.com>
2026-05-06 23:43:07 +0600

    feat: mocking data for test

--- learn-go-with-tests/fundamentals/main.go
+++ learn-go-with-tests/fundamentals/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"os"
 )
 
 func main() {
 	Greet(os.Stdout, "Elodie \n")
-	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper )
 }
