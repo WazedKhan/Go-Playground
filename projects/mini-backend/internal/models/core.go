68d9d82dd9c749a629c13a6dd1d3de8a86eac587
wazedkhan <wazedkhan119399@gmail.com>
2026-05-16 12:04:10 +0600

    fix: rach condition with mutex

--- projects/mini-backend/internal/models/core.go
+++ projects/mini-backend/internal/models/core.go
@@ -1,3 +1,16 @@
 package models
 
+import "time"
+
 type User map[string]string
+type RouteMetrics struct {
+	TotalDuration time.Duration `json:"total_duration"`
+	Count         int64         `json:"count"`
+}
+
+type Metrics struct {
+	TotalRequests int64                   `json:"total_requests"`
+	GetRequests   int64                   `json:"get_requests"`
+	PostRequests  int64                   `json:"post_requests"`
+	Latency       map[string]RouteMetrics `json:"latency"`
+}
