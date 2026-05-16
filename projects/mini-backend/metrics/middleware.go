81430c27a7c788c88ca99307d5dd7cbb5d403785
wazedkhan <wazedkhan119399@gmail.com>
2026-05-16 13:00:11 +0600

    feat: treaching request endpoint

--- projects/mini-backend/metrics/middleware.go
+++ projects/mini-backend/metrics/middleware.go
@@ -10,6 +10,7 @@ func LoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		start := time.Now()
+
 		fmt.Printf(
 			"[%s] %s %s \n",
 			start.Format("2006-01-02 03:04:05 PM"),
