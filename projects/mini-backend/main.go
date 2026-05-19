971b0009e79ae544649f9c62da7fb2da566cce1f
wazedkhan <wazedkhan119399@gmail.com>
2026-05-19 23:23:06 +0600

    feat: graceful shutdown - waiting option for go routine to complete it work - set wait limit to force kill the server - double ctrl to manualy kill ther server

--- projects/mini-backend/main.go
+++ projects/mini-backend/main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
@@ -25,37 +25,48 @@ func main() {
 	mux.HandleFunc("/health", middleware.GetHealth)
 	mux.HandleFunc("/metrics", middleware.RouteMetrics)
 
-	// to shutdown gracefully we need to allow main function to reach after server listen
-	// and to allow go signal layer we need to use goroutine as go reads codes from up to down
+	var wg sync.WaitGroup
+	const shutDownTimeOut = 25 * time.Second // found that kubernetes waits 30 before kill -9
 	port := ":8000"
 	middleware := middleware.LoggerMiddleware(
-		middleware.MetricsMiddleware(mux),
+		middleware.MetricsMiddleware(
+			middleware.TrackRequests(&wg, mux),
+		),
 	)
 	srv := &http.Server{
-		Addr: port,
+		Addr:    port,
 		Handler: middleware,
 	}
 
 	go func() {
-        log.Printf("Starting server on http://localhost%s/\n", port)
-        if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Printf("Starting server on http://localhost%s/\n", port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
-    }()
-	// now that we are running with go routine if we start the server it will close as main function completes before goroutine
-	// we need to wait for signal for quite
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<- quit
-	log.Println("Shutting down the server ....")
+	}()
 
-	// right now we are just waiting for the signal to quite then quitting the server but still following forceful shutdown
-	// set lets wait for some time to shutdown to give our server to handle its ongoing request/task
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
-    defer cancel()
+	// handling gracefully shutdown
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
-	if err := srv.Shutdown(ctx); err != nil{
-		log.Fatal("forced shutdown:", err)
+	<-ctx.Done()
+	log.Println("Shutting down... (Ctrl+C again to force)")
+	stop()
+
+	shutCtx, cancel := context.WithTimeout(context.Background(), shutDownTimeOut)
+	defer cancel()
+
+	go srv.Shutdown(shutCtx)
+	waitDone := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(waitDone)
+	}()
+
+	select {
+	case <-waitDone:
+		log.Println("all request done, clean exit")
+	case <-shutCtx.Done():
+		log.Printf("waited %q, forcing shutdown\n", shutDownTimeOut)
 	}
-	log.Println("server exited cleanly")
 }
